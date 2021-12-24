75a238318fbe22923adcb099f350aed4786317bd
TobaMan <72600776+TobaMan@users.noreply.github.com>
2021-12-24 14:24:05 +0100

    Update ServiceGuide.js

--- js/ServiceGuide.js
+++ js/ServiceGuide.js
@@ -515,7 +515,7 @@ function ShowLegend(){
 }
 
 
-function CheckChromeBrowser() {
+/* function CheckChromeBrowser() {
 	
 	var browserName = (function (agent) {
         switch (true) {
@@ -538,7 +538,44 @@ function CheckChromeBrowser() {
 		$(".chromebrowser").show();
 		return false;
 	}
+} */
+function CheckChromeBrowser() {
+    // Opera 8.0+
+    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    // Safari 3.0+ "[object HTMLElementConstructor]"
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) {
+            return p.toString() === "[object SafariRemoteNotification]";
+        })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+    // Internet Explorer 6-11
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
+    // Chrome 1 - 79
+    //var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    // Edge (based on chromium) detection
+    var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+    // Blink engine detection
+    var isBlink = (isChrome || isOpera) && !!window.CSS;
+    isChrome =
+        isFirefox == false &&
+        isSafari == false &&
+        isOpera == false &&
+        isIE == false &&
+        isEdge == false &&
+        isEdgeChromium == false;
+    if (isChrome) {
+        $(".chromebrowser").hide();
+        return true;
+    }
+    else {
+        $(".chromebrowser").show();
+        return false;
+    }
 }
+
 
 
 function FullScreen() {
